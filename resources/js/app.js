a1997ad9c7cfc909e327b903b264b8cf1b553316
alvaritodeveloper <desarrollalvarito@hotmail.com>
2024-03-27 18:13:36 -0400

    add sbadmin2 template

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import './bootstraps';
+
+import 'jquery'
+import 'bootstrap'
+import 'jquery.easing'
+import './sb-admin-2.min'
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
